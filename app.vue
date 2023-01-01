3d8064fa3fdb665581aa3dc35a149509f398d67f
musaubrian <musaubrian15@gmail.com>
2023-01-02 01:28:23 +0300

    chore: add form

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex flex-col w-full h-[80vh] items-center justify-center">
+    <h1 class="text-gray-800 text-3xl font-semibold capitalize mb-5 underline">TinieUrl</h1>
+    <form @submit.prevent="" class="md:w-3/6 w-5/6 flex flex-col items-center justify-center">
+      <input class="border-2 border-gray-800 rounded-lg text-center p-2 text-lg w-full" type="text"
+        placeholder="https://yourprettylonglinkhere.com">
+      <button type="submit"
+        class="w-4/6 md:w-3/6 border-0 p-3 bg-blue-500 md:mt-4 mt-5 text-gray-100 text-lg rounded-lg font-semibold transition-all active:bg-blue-700 active:transition-all">
+        Get your link
+      </button>
+    </form>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    modules: ['@nuxtjs/tailwindcss'],
+    app: {
+        head: {
+            title: 'TinieUrl',
+            meta: [
+                { charset: 'utf-8' },
+                {name: 'description', content: 'Url *shortener'},
+                { name: 'viewport', content: 'width=device-width, initial-scale=1.0'}
+            ],
+            link: [
+                {rel: 'stylesheet', type: 'text/css',
+                href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+            ]
+        }
+    }
 })
